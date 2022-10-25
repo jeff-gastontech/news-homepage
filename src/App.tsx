95533aaae3601347240e3fbaea3d39281913f78d
Jeff Goyette <jeff@gastontechnologies.com>
2022-10-25 14:05:49 -0400

    Updated with mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import Box from "@mui/material/Box";
 import ResponsiveAppBar from "./ResponsiveAppBar";
-import {Button, Divider, Link, Paper, Typography} from "@mui/material";
+import {
+    Button,
+    Divider, Drawer,
+    Link,
+    List,
+    ListItem,
+    ListItemButton, ListItemText,
+    Paper,
+    Typography,
+    useMediaQuery,
+    useTheme
+} from "@mui/material";
 import {Link as RouterLink, NavLink} from "react-router-dom";
 import Grid from '@mui/material/Unstable_Grid2';
 import Container from "@mui/material/Container";
@@ -12,47 +23,119 @@ import MainImage from "./assets/images/image-web-3-desktop.jpg";
 import ComputerImage from "./assets/images/image-retro-pcs.jpg";
 import KeyboardImage from "./assets/images/image-top-laptops.jpg";
 import ControllerImage from "./assets/images/image-gaming-growth.jpg";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 const activeClassName = "Active-Link";
+
+type Anchor = 'right';
 
 function checkIfActive(active: boolean) {
     return active ? activeClassName : "Link";
 }
 
 function App() {
+
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.up('sm'));
+
+    const [state, setState] = React.useState({
+        right: false,
+    });
+
+    const toggleDrawer =
+        (anchor: Anchor, open: boolean) =>
+            (event: React.KeyboardEvent | React.MouseEvent) => {
+                if (
+                    event.type === 'keydown' &&
+                    ((event as React.KeyboardEvent).key === 'Tab' ||
+                        (event as React.KeyboardEvent).key === 'Shift')
+                ) {
+                    return;
+                }
+
+                setState({ ...state, [anchor]: open });
+            };
+
+    const list = (anchor: Anchor) => (
+        <Box
+            sx={{ width: 250 }}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <List>
+                <ListItem>
+                    <Box sx={{marginLeft: "auto"}}>
+                        <IconButton onClick={toggleDrawer('right', false)}>
+                            <CloseIcon />
+                        </IconButton>
+                    </Box>
+                </ListItem>
+                {['Home', 'New', 'Popular', 'Trending', 'Categories'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+
     return (
-        <Box component={"main"} sx={{padding: "20px"}} flexGrow={1} maxWidth="80%" maxHeight="80%" margin="auto">
-            <Grid container spacing={2} alignItems="center">
-                <Grid xs={7}>
-                    <img src={Logo}/>
+        <Box component={"main"} sx={{padding: "20px"}} flexGrow={1} maxWidth="95%" maxHeight="95%" margin="auto">
+            <Grid container xs={12} spacing={2} justifyContent="space-between">
+                <Grid container xs={12} spacing={2} alignItems="center">
+                    <Grid xs={7}>
+                        <img src={Logo}/>
+                    </Grid>
+
+                    {matches ? (<React.Fragment>
+                        <Grid xs={1} textAlign="end">
+                            <NavLink end to={"/"} className={({isActive}) => checkIfActive(isActive)}>Home</NavLink>
+                        </Grid>
+                        <Grid xs={1} textAlign="end">
+                            <NavLink to={"/new"} className={({isActive}) => checkIfActive(isActive)}>New</NavLink>
+                        </Grid>
+                        <Grid xs={1} textAlign="end">
+                            <NavLink to={"/popular"} className={({isActive}) => checkIfActive(isActive)}>Popular</NavLink>
+                        </Grid>
+                        <Grid xs={1} textAlign="end">
+                            <NavLink to={"/trending"} className={({isActive}) => checkIfActive(isActive)}>Trending</NavLink>
+                        </Grid>
+                        <Grid xs={1} textAlign="end" paddingRight={0}>
+                            <NavLink to={"/categories"} className={({isActive}) => checkIfActive(isActive)}>Categories</NavLink>
+                        </Grid>
+                    </React.Fragment>) : (
+                        <React.Fragment>
+                            <Grid xs={5} textAlign="end">
+                                <IconButton onClick={toggleDrawer('right', true)}>
+                                    <MenuIcon />
+                                </IconButton>
+                            </Grid>
+                            <Drawer
+                                anchor={'right'}
+                                open={state['right']}
+                                onClose={toggleDrawer('right', false)}
+                            >
+                                {list('right')}
+                            </Drawer>
+                        </React.Fragment>
+                    )}
+
                 </Grid>
-                <Grid xs={1} textAlign="end">
-                    <NavLink end to={"/"} className={({isActive}) => checkIfActive(isActive)}>Home</NavLink>
-                </Grid>
-                <Grid xs={1} textAlign="end">
-                    <NavLink to={"/new"} className={({isActive}) => checkIfActive(isActive)}>New</NavLink>
-                </Grid>
-                <Grid xs={1} textAlign="end">
-                    <NavLink to={"/popular"} className={({isActive}) => checkIfActive(isActive)}>Popular</NavLink>
-                </Grid>
-                <Grid xs={1} textAlign="end">
-                    <NavLink to={"/trending"} className={({isActive}) => checkIfActive(isActive)}>Trending</NavLink>
-                </Grid>
-                <Grid xs={1} textAlign="end" paddingRight={0}>
-                    <NavLink to={"/categories"} className={({isActive}) => checkIfActive(isActive)}>Categories</NavLink>
-                </Grid>
-            </Grid>
-            <Grid container spacing={2} justifyContent="space-between">
-                <Grid container xs={8} paddingTop={0}>
+                <Grid container xs={12} md={7} paddingTop={0}>
                     <Grid xs={12} paddingRight={"20px"}>
                         <img src={MainImage}/>
                     </Grid>
-                    <Grid xs={6}>
+                    <Grid xs={12} md={6}>
                         <Typography variant="h1" sx={{fontWeight: "bold", color: "black"}}>
                             The Bright Future of Web 3.0?
                         </Typography>
                     </Grid>
-                    <Grid container xs={6}>
+                    <Grid container xs={12} md={6}>
                         <Grid xs={12} className="Regular-Text">
                             <Typography variant="h5">
                                 We dive into the next evolution of the web that claims to put the power of the platforms
@@ -60,15 +143,15 @@ function App() {
                                 But is it really fulfilling its promise?
                             </Typography>
                         </Grid>
-                        <Grid xs={12}>
-                            <Button variant="contained" className="Button">
+                        <Grid xs={12} style={matches ? {position: "relative"} : {paddingLeft: 0, paddingRight: 0}}>
+                            <Button variant="contained" className={matches ? "Button" : "Button-Full"} style={matches ? {position: "absolute",left: "8px", right: "8px"} : {}}>
                                 Read more
                             </Button>
                         </Grid>
                     </Grid>
                 </Grid>
-                <Grid container xs={4}>
-                    <Paper className="New-Articles">
+                <Grid container xs={12} md={4}>
+                    <Paper id="articles" className="New-Articles">
                         <Grid xs={12} paddingTop={"8px"} paddingLeft={"8px"}>
                             <Typography variant="h3" className={"New-Article-Heading"}>
                                 New
@@ -113,7 +196,7 @@ function App() {
                     </Paper>
                 </Grid>
                 <Grid container xs={12} spacing={2} paddingTop={8}>
-                    <Grid container xs={4}>
+                    <Grid container xs={12} md={4}>
                         <Grid xs={3}>
                             <img src={ComputerImage}/>
                         </Grid>
@@ -125,7 +208,7 @@ function App() {
                                 old PCs are given modern upgrades?</Typography></Grid>
                         </Grid>
                     </Grid>
-                    <Grid container xs={4}>
+                    <Grid container xs={12} md={4}>
                         <Grid xs={3}>
                             <img src={KeyboardImage}/>
                         </Grid>
@@ -139,9 +222,9 @@ function App() {
                                 various needs and budgets.</Typography></Grid>
                         </Grid>
                     </Grid>
-                    <Grid container xs={4}>
+                    <Grid container xs={12} md={4}>
                         <Grid xs={3}>
-                            <img src={ControllerImage}/>
+                            <img src={ControllerImage} style={{objectFit:"fill"}}/>
                         </Grid>
                         <Grid container xs={8}>
                             <Grid xs={12}><Typography variant="h2" color="grey.500">03</Typography>
